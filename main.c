ca1977704a552d8b6d02aaf5bba724703183c69a
Alles Rebel <allesrebel@yahoo.com>
2014-04-08 01:17:13 -0700

    Refactored code - writing core functions

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <msp430.h>
 
 //Bool
-#define bool 	char
-#define true 	1
-#define false 	0
+#include <stdbool.h>
 
 /*
  * TODO:	Should make the library take in a clock speed and base timings
@@ -31,49 +29,23 @@
 #define	RS 	BIT2
 #define	RW	BIT1
 #define EN	BIT0
+#define D7	BIT7
+#define DB	BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7
 
 /*
  * Functions that LCD Library Will Have
+ * Function descriptions at implementation!
  */
+//CORE Functions (TODO: Write some tests for these!)
+void lcd_sendEnable();
+void lcd_instWrite(char data);
+char lcd_readBFandAC();
+void lcd_dataWrite(char data);
+char lcd_dataRead();
 
-/*
- * Busy Check -
- * 	Params - int* ACaddr, passes the location of Address counter if given (optional)
- * 	Returns true if busy!
- * 	Return false if ready for next instruction!
- */
-int lcd_isbusy(int* ac_addr) {
-	// Set direction of Pins (both RS, R/W as outputs)
-	P2DIR |= RS | RW;
-	return true;
-}
-
-/*
- * sendEnable - Toggles enable to process instruction/data
- * 	-note LCD relies on this to actually clock the data in
- * 	-note Have Data ready prior to calling this
- */
-void sendEnable() {
-	//Set Enable to output
-	P2DIR |= EN;
-	//Set Enable to output logical HIGH
-	P2OUT |= EN;
-
-	//Hold for atleast 230 ns (~4cycles at 16Mhz)
-	__delay_cycles(4);
-
-	//Set Enable to output Logical LOW
-	P2OUT &= (~EN);
-}
-
-/*
- * Do the init cycle for the display
- *	Gets the display into 2 line mode and sets up
- *	Everything else
- */
-void init_display() {
-
-}
+//Derived Functions
+void lcd_init();
+int lcd_isbusy();
 
 /*
  * main.c
@@ -90,4 +62,85 @@ int main(void) {
 	DCOCTL = CALDCO_16MHZ;
 
 	return 0;
+}
+
+/*
+ * lcd_readBFandAC - Core Function that
+ * 	Reads the Busy flag and along with where the
+ * 	address counter is currently at.
+ * 	returns - char with BF | AC 6 downto 0
+ */
+char lcd_readBFandAC() {
+	// Set direction of Pins (both RS, R/W as outputs)
+	P2DIR |= RS | RW;
+	// Set Direction of DataBus pins (D0 - D7) to input
+	P1DIR &= (~DB);
+
+	//Set CMD pins (RS,RW) to (0,1) for read mode
+	P2OUT &= (~RS);
+	P2OUT |= RW;
+
+	//Let LCD Process Command
+	lcd_sendEnable();
+
+	return P1IN;
+}
+
+/*
+ * lcd_instWrite - Core Function that
+ * 	Writes to the instruction register of the
+ * 	lcd controller.
+ *
+ * 	param - char of the data bus
+ */
+void lcd_instWrite(char data) {
+	// Set direction of Pins (both RS, R/W as outputs)
+	P2DIR |= RS | RW;
+	// Set Direction of DataBus pins (D0 - D7) to output
+	P1DIR |= DB;
+}
+
+/*
+ * lcd_sendEnable - Toggles enable to process instruction/data
+ * 	-note LCD relies on this to actually clock the data in
+ * 	-note Have Data ready prior to calling this
+ */
+void lcd_sendEnable() {
+	//Set Enable to output
+	P2DIR |= EN;
+	//Set Enable to output logical HIGH
+	P2OUT |= EN;
+
+	//Hold for atleast 230 ns (~4cycles at 16Mhz)
+	__delay_cycles(4);
+
+	//Set Enable to output Logical LOW
+	P2OUT &= (~EN);
+}
+
+/*
+ * lcd_isbusy - Busy Check
+ * 	Params - int* ACaddr, passes the location of Address counter if given (optional)
+ * 	Returns true if busy!
+ * 	Return false if ready for next instruction!
+ */
+int lcd_isbusy() {
+	char input = lcd_readBFandAC();
+
+	//Busy Flag is on D7, read that in
+	//if it's high => busy
+	if ((P2IN & D7)== D7)
+		return true;
+	else
+		return false;
+}
+
+/*
+ * lcd_init - Do the init cycle for the display
+ *	Gets the display into 2 line mode and sets up
+ *	Everything else
+ *	TODO: allow passing in options to set them (bitwise maybe)
+ */
+void lcd_init() {
+
 }
